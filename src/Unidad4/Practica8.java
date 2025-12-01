def48535d4112e0a34b33e7f0b2d5e2db6bd603e
USR <USR@DESKTOP-OU9CLSO>
2025-12-01 16:32:10 -0600

    confucion en los if

---
+++ src/Unidad4/Practica8.java
@@ -0,0 +1,41 @@
+
+package Unidad4;
+
+import java.util.Scanner;
+
+public class Practica8 {
+    public static void main(String[] args) {
+        Scanner leer = new Scanner (System.in);
+        int ti;
+        String di, tu;
+        float pag,im, tot,de;
+        
+        System.out.println("CUANTO TIEMPO HABLO POR TELEFONO(SEGUNDOS): ");
+        ti=leer.nextInt();
+        System.out.println("QUE DIA ES HOY? ");
+        
+        if (ti<5) {
+            pag=1.00f;
+        } else {
+            if (ti<8) {
+                pag=0.80f;
+            } else
+                if (ti<10) {
+                pag=0.70f;
+            } else {
+                    pag=0.50f;
+            }
+        }
+        if (di.equals("D")) {
+            de=pag*0.03f;
+        } else {
+            if (tu.equals("M")) {
+                de=pag*0.15f;
+            } else {
+                de=pag*0.10f;
+            }
+        }
+        
+    }
+    
+}
