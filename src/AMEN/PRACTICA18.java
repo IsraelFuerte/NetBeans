9deb5cc23ecf4b50d5a819c91e00d514805e1d73
USR <USR@DESKTOP-OU9CLSO>
2025-12-18 19:14:17 -0600

    PRACTICA NO° 18

---
+++ src/AMEN/PRACTICA18.java
@@ -0,0 +1,16 @@
+package AMEN;
+
+public class PRACTICA18 {
+    public static void main(String[] args) {
+        
+        int fila=6;
+        for (int i = 1; i <= fila; i++) {
+    for (int j = 0; j<i; j++) {
+                System.out.print(j+2);
+            }
+            System.out.println("");
+        }
+        
+    }
+    
+}
