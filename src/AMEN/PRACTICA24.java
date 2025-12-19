949887faaa7034b0c774e9ccc2318336cedd63ff
USR <USR@DESKTOP-OU9CLSO>
2025-12-18 18:25:23 -0600

    PRACTICA NO° 24

---
+++ src/AMEN/PRACTICA24.java
@@ -0,0 +1,14 @@
+package AMEN;
+
+public class PRACTICA24 {
+    public static void main(String[] args) {
+        
+        for (int tabla = 3; tabla <=3 ; tabla++) {
+            for (int i = 1; i < 11; i++) {
+                System.out.println(tabla+ "x" + i + "="  +tabla*i);
+            }
+        }
+        
+    }
+    
+}
